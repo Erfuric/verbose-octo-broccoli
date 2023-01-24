5764fb1dd90105c10b8a7208f85b93a369af89c9
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-01-24 22:57:07 +1100

    functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
 // global variables for site
-var apiKey = "1c02a9178871e0c579bf11abd8cecbbb";
-var previousSearch = [];
+const apiKey = "1c02a9178871e0c579bf11abd8cecbbb";
+const forecastUrlStart = 'http://api.openweathermap.org/data/2.5/forecast?q='
+const forecastUrlEnd = '&appid='
 
+// Add event listener to form
+document.getElementById("city-search").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var cityInput = document.getElementById("city").value;
+    var forecastUrl = forecastUrlStart + cityInput + forecastUrlEnd + apiKey;
+    // Use fetch to make API call to OpenWeatherMap API
+    fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {
+        const forecast = data.list;
+        // Get the div where the forecast will be displayed
+        const forecastDiv = document.getElementById("five-day");
+        // Clear the div before displaying new forecast
+        forecastDiv.innerHTML = "";
+        for (let i = 0; i < forecast.length; i++) {
+            // Create a new div for each forecast
+            const forecastElem = document.createElement("div");
+            forecastElem.innerHTML = forecast[i].dt_txt + ": " + forecast[i].main.temp + "F";
+            // Append the forecast to the forecast div
+            forecastDiv.appendChild(forecastElem);
+        }
+    })
+    .catch(error => console.log(error));
+});
